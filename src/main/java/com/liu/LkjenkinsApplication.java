7fbb54ebe487ad5b689203d2fa6721d61f33065b
liukailk <1016834568@qq.com>
2020-01-20 17:17:36 +0800

    4

--- src/main/java/com/liu/LkjenkinsApplication.java
+++ src/main/java/com/liu/LkjenkinsApplication.java
@@ -2,9 +2,10 @@ package com.liu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LkjenkinsApplication {
+public class LkjenkinsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LkjenkinsApplication.class, args);
